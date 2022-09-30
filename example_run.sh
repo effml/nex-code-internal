c1009f97cc5e7b42d4addbe8e3108fe951dc96dc
Stephen Yang <jiezhi@berkeley.edu>
2022-09-29 17:48:36 -0700

    radii pugin working, minor improvement

--- example_run.sh
+++ example_run.sh
@@ -8,7 +8,7 @@
 #SBATCH -n 1
 #SBATCH -t 2-00:00
 #SBATCH --cpus-per-task 10
-#SBATCH  --gres gpu:8
+#SBATCH  --gres gpu:1
 #SBATCH --nodelist=luigi
 
 pwd
@@ -24,4 +24,4 @@ echo starting nex training:
 date
 
 
-python ~/nex-code-internal/train.py -scene /data/jiezhi/crest_demo -model_dir crest -http -layers 12 -sublayers 6 -hidden 256 -tb_saveimage 50 -tb_savempi 3
+python ~/nex-code-internal/train.py -scene /data/jiezhi/crest_demo -model_dir crest -http -layers 10 -sublayers 4 -hidden 256 -tb_saveimage 50 -tb_savempi 3
